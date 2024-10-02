019af5380aef856ad40f0de7ee752c95b383ffda
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-03 01:59:40 +0200

    test production

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:4000',
-    headers: {
-      'Content-Type': 'application/json'
-    }
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://sos-six-phi.vercel.app' : 'http://localhost:4000',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
 export const getData = async() =>{
