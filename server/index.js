d537d6bc8b5d42a3cfb11e29c9dbc168675cef7c
MiguelGMata <miguelgmata.dev@gmail.com>
2024-10-02 23:50:00 +0200

    test production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ require('dotenv').config();
 const studiantRoutes = require('./src/routes/studentRoutes');
 const teacherRoutes = require('./src/routes/teacherRoutes');
 
-app.use(cors());
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://sos-six-phi.vercel.app/',
+}));
 
 const port = process.env.APP_PORT || 3000
 app.get('/', (req, res) => {
